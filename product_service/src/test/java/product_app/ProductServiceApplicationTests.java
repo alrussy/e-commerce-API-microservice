401e6d85cad8c72080eb922232beb89dea0d21aa
Ahmed Alrussy <alrussywe1@gmail.com>
2025-05-01 18:01:46 +0300

    main

--- product_service/src/test/java/product_app/ProductServiceApplicationTests.java
+++ product_service/src/test/java/product_app/ProductServiceApplicationTests.java
@@ -8,8 +8,6 @@ import org.springframework.context.annotation.Import;
 @SpringBootTest
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
